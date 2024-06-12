1c5c12f3923cd7ad370971935f5cd63fcc0dd2e2
Aleksandra <aleksandra.lacinska@gmail.com>
2024-06-12 21:50:24 +0200

    przekierowanie z journal na calendar

--- app/src/main/java/com/example/dziennikmvvm/view/JournalActivity.kt
+++ app/src/main/java/com/example/dziennikmvvm/view/JournalActivity.kt
@@ -1,6 +1,9 @@
 package com.example.dziennikmvvm.view
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.example.dziennikmvvm.R
 
@@ -8,5 +11,24 @@ class JournalActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_journal)
+        val toolbar = findViewById<androidx.appcompat.widget.Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_journal, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_right_icon -> {
+                // Start CalendarActivity when the icon is clicked
+                val intent = Intent(this, CalendarActivity::class.java)
+                startActivity(intent)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
