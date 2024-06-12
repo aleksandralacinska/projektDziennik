7d5681bdf83e2f952dc787fd65bd43dd297b30e3
Aleksandra <aleksandra.lacinska@gmail.com>
2024-06-12 18:43:45 +0200

    trzeci ekran w aplikacji

--- app/src/main/java/com/example/dziennikmvvm/view/HomeActivity.kt
+++ app/src/main/java/com/example/dziennikmvvm/view/HomeActivity.kt
@@ -1,6 +1,9 @@
 package com.example.dziennikmvvm.view
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 import com.example.dziennikmvvm.R
 
@@ -8,5 +11,12 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        // Przekierowanie po 3 sekundach do ekranu Journal
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, JournalActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
     }
 }
