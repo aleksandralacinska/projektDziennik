1c02103a6d8f5559c2c45ce5055da319c1a348c3
ViniestPlane76 <karolmuzinski@gmail.com>
2024-06-13 18:59:38 +0200

    bar i nawigacja powrotu z kalendarza

--- app/src/main/java/com/example/dziennikmvvm/view/CalendarActivity.kt
+++ app/src/main/java/com/example/dziennikmvvm/view/CalendarActivity.kt
@@ -2,14 +2,24 @@ package com.example.dziennikmvvm.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import com.applandeo.materialcalendarview.CalendarView
-import java.util.*
 import com.example.dziennikmvvm.R
+import java.util.*
+
 class CalendarActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calendar)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        // Obsługa kliknięcia przycisku "powrót"
+        toolbar.setNavigationOnClickListener {
+            finish()
+        }
 
         val calendarView = findViewById<CalendarView>(R.id.calendarView)
 
